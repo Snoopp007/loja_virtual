edcec4e9b2bda169ab0265ac48c5fe0d7475a171
Richard Ashley <richardashley440@yahoo.co.uk>
2018-10-13 02:37:02 -0300

    Configuração do site

--- index.php
+++ index.php
@@ -2,18 +2,20 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Loja\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Loja\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
+	$page->setTpl("index");
 
-	echo json_encode($results); 
-
+	
 });
 
 $app->run();
