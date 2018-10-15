b953cc8d470287d89c1d95c594cd48132e21badd
Richard Ashley <richardashley440@yahoo.co.uk>
2018-10-15 02:37:10 -0300

    criação page Admin

--- vendor/loja/php-classes/src/Page.php
+++ vendor/loja/php-classes/src/Page.php
@@ -5,7 +5,6 @@ namespace Loja;
 use Rain\Tpl;
 
 class Page {
-
 	private $tpl;
 	private $options = [];
 	private $defaults = [
@@ -13,57 +12,32 @@ class Page {
 		"footer"=>true,
 		"data"=>[]
 	];
-
-	public function __construct($opts = array())
-	{
-
+	public function __construct($opts = array(), $tpl_dir = "/views/"){
+		
 		$this->options = array_merge($this->defaults, $opts);
-
 		$config = array(
-		    "base_url"      => null,
-		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/",
-		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
-		    "debug"         => false
-		);
-
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"         => false
+	    );
 		Tpl::configure( $config );
-
-		$this->tpl = new Tpl();
-
-		if ($this->options['data']) $this->setData($this->options['data']);
-
-		if ($this->options['header'] === true) $this->tpl->draw("header", false);
-
+		$this->tpl = new Tpl;
+		$this->setData($this->options["data"]);
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 	}
-
-	public function __destruct()
-	{
-
-		if ($this->options['footer'] === true) $this->tpl->draw("footer", false);
-
-	}
-
 	private function setData($data = array())
 	{
-
-		foreach($data as $key => $val)
-		{
-
-			$this->tpl->assign($key, $val);
-
+		foreach ($data as $key => $value) {
+			$this->tpl->assign($key, $value);
 		}
-
 	}
-
-	public function setTpl($tplname, $data = array(), $returnHTML = false)
+	public function setTpl($name, $data = array(), $returnHTML = false)
 	{
-
 		$this->setData($data);
-
-		return $this->tpl->draw($tplname, $returnHTML);
-
+		return $this->tpl->draw($name, $returnHTML);
 	}
-
+	public function __destruct(){
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
+	}
 }
-
  ?>
