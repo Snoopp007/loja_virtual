35e17e64a32559188441cdfdb587baf533794e0c
Richard Ashley <richardashley440@yahoo.co.uk>
2018-10-21 00:54:26 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,35 +1,63 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
+require_once("functions.php");
 
-use \Slim\Slim;
-use \Loja\Page;
-use \Loja\PageAdmin;
+use Loja\Model\User;
 
-
-$app = new Slim();
+$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page = new Page();
+	$page = new Loja\Page();
 
 	$page->setTpl("index");
 
-	
 });
-
 
 $app->get('/admin', function() {
     
-	$page = new PageAdmin();
+	User::verifyLogin();
+
+	$page = new Loja\PageAdmin();
 
 	$page->setTpl("index");
 
-	
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Loja\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
 });
 
 $app->run();
+
 
  ?>
--- vendor/loja/php-classes/src/Page.php
+++ vendor/loja/php-classes/src/Page.php
@@ -5,6 +5,7 @@ namespace Loja;
 use Rain\Tpl;
 
 class Page {
+
 	private $tpl;
 	private $options = [];
 	private $defaults = [
@@ -12,32 +13,59 @@ class Page {
 		"footer"=>true,
 		"data"=>[]
 	];
-	public function __construct($opts = array(), $tpl_dir = "/views/"){
-		
+
+	public function __construct($opts = array(), $tpl_dir = "/views/")
+	{
+
+		$this->defaults["data"]["session"] = $_SESSION;
+
 		$this->options = array_merge($this->defaults, $opts);
+
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
-			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-			"debug"         => false
-	    );
+		    "base_url"      => null,
+		    "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].$tpl_dir,
+		    "cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
+		    "debug"         => false
+		);
+
 		Tpl::configure( $config );
-		$this->tpl = new Tpl;
-		$this->setData($this->options["data"]);
-		if ($this->options["header"] === true) $this->tpl->draw("header");
+
+		$this->tpl = new Tpl();
+
+		if ($this->options['data']) $this->setData($this->options['data']);
+
+		if ($this->options['header'] === true) $this->tpl->draw("header", false);
+
 	}
+
+	public function __destruct()
+	{
+
+		if ($this->options['footer'] === true) $this->tpl->draw("footer", false);
+
+	}
+
 	private function setData($data = array())
 	{
-		foreach ($data as $key => $value) {
-			$this->tpl->assign($key, $value);
+
+		foreach($data as $key => $val)
+		{
+
+			$this->tpl->assign($key, $val);
+
 		}
+
 	}
-	public function setTpl($name, $data = array(), $returnHTML = false)
+
+	public function setTpl($tplname, $data = array(), $returnHTML = false)
 	{
+
 		$this->setData($data);
-		return $this->tpl->draw($name, $returnHTML);
+
+		return $this->tpl->draw($tplname, $returnHTML);
+
 	}
-	public function __destruct(){
-		if ($this->options["footer"] === true) $this->tpl->draw("footer");
-	}
+
 }
+
  ?>
